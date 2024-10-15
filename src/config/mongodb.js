c8787a89ea866a89624d05693f74739a5d70bb99
al6894 <usandylee@gmail.com>
2024-10-15 15:27:29 -0400

    Changed mongodb.js to use mongoose over native drivers.

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,21 +1,16 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://al6894:UkcleS9JPNN1qj2J@designproject.7q4va.mongodb.net/?retryWrites=true&w=majority&appName=DesignProject";
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-export async function run() {
+const connectDB = async () => {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected.');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
-}
+};
+
+module.exports = connectDB;
